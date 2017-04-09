47cee528dac0f163d7dcafe536f176e71bc0a73d
liujianyong <liujianyong@myhome.163.com>
2017-04-09 23:04:17 +0800

    observer patter

---
+++ src/main/java/responsibility/observer/Observer.java
@@ -0,0 +1,11 @@
+package responsibility.observer;
+
+public interface Observer {
+    /**
+     * 更新接口
+     * 
+     * @param state
+     *            更新的状态
+     */
+    void update(String state);
+}
